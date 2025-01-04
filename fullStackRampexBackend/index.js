ee5827edaa3aec0f5ac4111be9c177e91f43d3cd
arumpugazl <sankararumpugazl@gmail.com>
2025-01-04 16:10:10 +0530

    Backend commit

--- fullStackRampexBackend/index.js
+++ fullStackRampexBackend/index.js
@@ -1,12 +1,20 @@
 var express = require('express')
 var path = require('path')
 var mdb = require('mongoose')
+var cors = require('cors')
 var User = require('./models/users')
 var app = express()
-const PORT = 3001;
-app.use(express.json())
+var env=require('dotenv')
 
-mdb.connect("mongodb://localhost:27017").then(() => {
+const PORT = 3001;
+
+env.config()
+app.use(express.json())
+app.use(cors())
+
+
+mdb.connect(process.env.MONGO_URL).then(() => {//mongodb://localhost:27017
+    console.log("process.env.MONGO_URL")
     console.log("MonoDB connection Successful");
 }).catch(() => {
     console.log("check your connection string");
@@ -25,14 +33,17 @@ app.get('/static', (req, res) => {
 
 app.post('/signup', (req, res) => {
     console.log(req.body);
-    var { firstName, lastName, email } = req.body
-    console.log(firstName, lastName, email);
+    var { firstName, lastName, email, password } = req.body
+    console.log(firstName, lastName, email, password);
     try {
-        var newUser = new User({
-            firstName: firstName,
-            lastName: lastName,
-            email: email
-        })
+        // var newUser = new User({
+        //     firstName: firstName,
+        //     lastName: lastName,
+        //     email: email,
+        //     password:password
+        //})
+        var newUser = new User(req.body)
+        console.log(req.body.password);
         newUser.save()
         console.log("User Added Successfully");
         res.status(200).send("User  Added successfully")
@@ -42,17 +53,42 @@ app.post('/signup', (req, res) => {
     }
 
 })
-app.get('/getsignup',async(req,res)=>{
-    try{
-        var allSignUpRecords=await User.find()
+app.get('/getsignup', async (req, res) => {
+    try {
+        var allSignUpRecords = await User.find()
         res.json(allSignUpRecords)
         console.log("All data fetched");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         res.send(err)
     }
 })
+app.post('/login', async (req, res) => {
+    var { email, password } = req.body
+    try {
+        var existingUser = await User.findOne({ email: email })
+        //console.log(existingUser);
+        if (existingUser) {
+            if (existingUser.password !== password) {
+                res.json({ message: "Invalid Credentials", isLoggedIn: false })
+            }
+            else {
+                res.json({ message: "Login Successful ", isLoggedIn: true })
+            }
+        }
+        else {
+            //console.log("Inside else")
+            res.json({ message: "Login Failed", isLoggedIn: false })
+        }
+
+    }
+    catch (err) {
+        console.log(err); //console.log("Login failed")
+    }
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Backend Server Started\nUrl:http://localhost:${PORT}`)
