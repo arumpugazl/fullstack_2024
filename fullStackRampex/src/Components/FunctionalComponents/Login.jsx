b6e77035c4f85df7521718a37609880f829a9071
arumpugazl <sankararumpugazl@gmail.com>
2025-01-04 16:11:09 +0530

    Frontend login commit

--- fullStackRampex/src/Components/FunctionalComponents/Login.jsx
+++ fullStackRampex/src/Components/FunctionalComponents/Login.jsx
@@ -1,20 +1,37 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+  var navigate=useNavigate();
+  
+var Login = async(event) => {
+    event.preventDefault()
+    var req = await axios.post("http://localhost:3001/login", {
+        email: email,
+        password: password
+    })
+    console.log(req.data);
+    var isLoginSuccessful =req.data.isLoggedIn
+    console.log(isLoginSuccessful)
+    if(isLoginSuccessful){
+        navigate('/Home')
+        var message=req.data.message
+        console.log(message);
+    }
+    else{
+        alert("Wrong Crdentials");
+    }
+    
+}
 
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={Login}>
         <div>
           <label>Email:</label>
           <input
--- fullStackRampex/src/Components/FunctionalComponents/Signup.jsx
+++ fullStackRampex/src/Components/FunctionalComponents/Signup.jsx
@@ -1,53 +1,75 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
-
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 function Signup() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [firstName, setfirstName] = useState('');
+    const [lastName, setlastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    var navigate=useNavigate();
 
-  const handleSignup = (e) => {
-    e.preventDefault();
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
-
-  return (
-    <div>
-      <h1>Signup</h1>
-      <form onSubmit={handleSignup}>
+    //   const handleSignup = (e) => {
+    //     e.preventDefault();
+    //     console.log('Name:', name);
+    //     console.log('Email:', email);
+    //     console.log('Password:', password);
+    // };
+    var signup = async(event) => {
+        event.preventDefault()
+        var req = await axios.post("http://localhost:3001/signup", {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        })
+        console.log(req);
+        navigate('/login')
+    }
+    return (
         <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
+            <h1>Signup</h1>
+            <form onSubmit={signup}>
+                <div>
+                    <label>FirstName:</label>
+                    <input
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => setfirstName(e.target.value)}
+                        required
+                    />
+                </div><br></br>
+                <div>
+                    <label>LastName:</label>
+                    <input
+                        type="text"
+                        value={lastName}
+                        onChange={(e) => setlastName(e.target.value)}
+                        required
+                    />
+                </div><br></br>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div><br></br>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div><br></br>
+                <button type="submit">Signup</button>
+            </form>
         </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Signup</button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default Signup;
